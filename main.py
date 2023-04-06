b8cde3f9fcfd781c540587b50c8c18d674de7d77
wearetyomsmnv <a7n000v+1@gmail.com>
2023-04-06 17:10:24 +0300

    version 1.1.0

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import argparse
 from bs4 import BeautifulSoup
 import sys
 from termcolor import cprint
+from alive_progress import alive_bar
 
 
 class Bcolors:
@@ -36,7 +37,7 @@ def print_banner():
 
 print_banner()
 
-print(Bcolors.HEADER + 'build 1.0.0')
+print(Bcolors.HEADER + 'build 1.1.0')
 print(Bcolors.HEADER + 'GPT-based web-dir fuzzer')
 print(Bcolors.HEADER + '@wearetyomsmnv')
 print(Bcolors.OKGREEN + 'for web penetration testers with <3')
@@ -46,12 +47,20 @@ print(Bcolors.FAIL)
 commands = argparse.ArgumentParser(description='GPT-based directory fuzzer')
 commands.add_argument('link', help='Укажите ссылку на веб-ресурс')
 commands.add_argument('api_key', help='Укажите api-key для chat-gpt')
+commands.add_argument('num', help='Укажите колличество слов для словаря[1-50](Не всегда отрабатывает)')
+commands.add_argument('--insecure', action='store_true', help='поиск небезопасных директорий')
+commands.add_argument('--backup', action='store_true', help='поиск бекапов')
 
 
 args1 = commands.parse_args()
 
 link = args1.link
 api_key = args1.api_key
+insecure = args1.insecure
+backups = args1.backup
+num = args1.num
+
+
 
 
 if not any([link, api_key]):
@@ -59,6 +68,7 @@ if not any([link, api_key]):
     print(Bcolors.FAIL + "[NOTE:] " + 'Попробуйте ещё раз')
     sys.exit()
 
+openai.api_key = api_key
 
 def check_wordpress(linked):
     response = requests.get(f"{linked}wp-login.php")
@@ -69,6 +79,16 @@ def check_wordpress(linked):
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
         if soup.find("meta", {"name": "generator", "content": "WordPress"}):
+            return True
+
+    return False
+
+
+def check_woocommerce(linked):
+    response = requests.get(linked)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        if soup.find("meta", {"name": "generator", "content": "WooCommerce"}):
             return True
 
     return False
@@ -106,6 +126,16 @@ def check_drupal(linked):
     return False
 
 
+def check_shopify(linked):
+    response = requests.get(linked)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        if soup.find("meta", {"name": "generator", "content": "Shopify"}):
+            return True
+
+    return False
+
+
 def check_1c_bitrix(linked):
     response = requests.get(f"{linked}bitrix/")
     if response.status_code == 200:
@@ -114,6 +144,7 @@ def check_1c_bitrix(linked):
             return True
 
     return False
+
 
 def detect_cms(linked):
     response = requests.get(linked)
@@ -124,6 +155,18 @@ def detect_cms(linked):
         soup.find_all("link", rel="stylesheet", href=lambda href: href and "wp-content" in href) or \
         soup.find("script", {"id": "wp-emoji-release"}):
         return "WordPress"
+
+    # Check for WooCommerce
+    if soup.find("meta", {"name": "generator", "content": "WooCommerce"}) or \
+        soup.find_all("link", rel="stylesheet", href=lambda href: href and "woocommerce" in href) or \
+        soup.find("script", {"src": lambda src: src and "woocommerce" in src}):
+        return "WooCommerce"
+
+    # Check for shopify
+    if soup.find("meta", {"name": "generator", "content": "Shopify"}) or \
+        soup.find_all("link", rel="stylesheet", href=lambda href: href and "shopify" in href) or \
+        soup.find("script", {"src": lambda src: src and "shopify" in src}):
+        return "Shopify"
 
     # Check for Joomla
     elif soup.find("meta", {"name": "generator", "content": "Joomla!"}) or \
@@ -144,18 +187,16 @@ def detect_cms(linked):
         return "CMS не определена"
 
 
-
 detected_cms = detect_cms(link)
 print(Bcolors.OKGREEN + f"Detected CMS: {detected_cms}")
 
 
-def gpt(cms, key):
-    openai.api_key = key
+def gpt(cms):
 
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=(
-            f"Сгенерируй пожалуйста список директорий и файлов для CMS {cms}, которые ты знаешь. "
+            f"Сгенерируй пожалуйста большой список директорий и файлов для CMS {cms}, которые ты знаешь. "
             f"Просто выведи список директорий без своих пояснений.\n"
         ),
         temperature=0.5,
@@ -168,27 +209,106 @@ def gpt(cms, key):
 
     status = response.choices[0].text.split('\n')
 
-    result_dict = {i: item.strip().lstrip('- ').replace('/', '').replace('\n', '') for i, item in enumerate(status) if item.strip()}
+    result_dict = {i: '/'.join([s.strip() for s in item.split('/')]).replace('//', '/') for i, item in enumerate(status) if item.strip()}
 
-    print("Словарь готов")
+    print(Bcolors.OKCYAN + "[+]: " + "СЛОВАРЬ ГОТОВ")
 
     return result_dict
 
+directories_dict = gpt(detected_cms)
 
 def check_files(dictionary_dir, link):
     directories = []
-    for key, directory in dictionary_dir.items():
-        url = f"{link.lstrip('/')}{directory}"
-        response = requests.get(url)
-        if response.status_code == 200:
-            directories.append(f"{Bcolors.OKGREEN}[+]{Bcolors.ENDC} {key}: {url}")
-        else:
-            directories.append(f"{Bcolors.FAIL}[-]{Bcolors.ENDC} {key}: {url}")
+    print(Bcolors.OKCYAN + "[+]: "+"ИДЁТ ПРОВЕРКА")
+    with alive_bar(len(dictionary_dir)) as bar:
+        for key, directory in dictionary_dir.items():
+            url = f"{link.lstrip('/')}{directory}"
+            response = requests.get(url)
+            if response.status_code == 200:
+                directories.append(f"{Bcolors.OKGREEN}[+]{Bcolors.ENDC} {key}: {url}")
+            else:
+                directories.append(f"{Bcolors.FAIL}[-]{Bcolors.ENDC} {key}: {url}")
+            bar()
     return directories
 
-directories_dict = gpt(detected_cms, api_key)
+
 
 
 results = check_files(directories_dict, link)
 for result in results:
     print(result)
+
+if insecure:
+    print(Bcolors.OKGREEN + "[+]: "+"CHECK INSECURE DIR")
+
+
+    def gpt_insecure(cms):
+
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=(
+                f"Сгенерируй пожалуйста большой список небезопасных директорий и небезопасных файлов для CMS {cms}, которые ты знаешь. "
+                f"Просто выведи список директорий без своих пояснений.\n"
+            ),
+            temperature=0.5,
+            max_tokens=2048,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=["\n\n"],
+        )
+
+        status = response.choices[0].text.split('\n')
+
+        result_dict = {i: '/'.join([s.strip() for s in item.split('/')]).replace('//', '/') for i, item in enumerate(status) if item.strip()}
+
+
+
+        print(Bcolors.OKCYAN + "[+]: " + "СЛОВАРЬ ГОТОВ")
+
+        return result_dict
+
+
+    directories_dict = gpt_insecure(detected_cms)
+
+    results_insecure = check_files(directories_dict, link)
+    for result in results_insecure:
+        print(result)
+
+if backups:
+    print(Bcolors.OKGREEN + "[+]: "+"CHECK BACKUP FILES")
+
+
+    def gpt_insecure(cms):
+
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=(
+                f"Сгенерируй пожалуйста большой список файлов, которые могут являться бэкапами для CMS {cms},"
+                f" которые ты знаешь. "
+                f"Просто выведи список директорий без своих пояснений.\n"
+            ),
+            temperature=0.5,
+            max_tokens=2048,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=["\n\n"],
+        )
+
+        status = response.choices[0].text.split('\n')
+
+        result_dict = {i: '/'.join([s.strip() for s in item.split('/')]).replace('//', '/') for i, item in enumerate(status) if item.strip()}
+
+
+
+        print(Bcolors.OKCYAN + "[+]: " + "СЛОВАРЬ ГОТОВ")
+
+        return result_dict
+
+
+    directories_dict = gpt_insecure(detected_cms)
+
+    results_backups = check_files(directories_dict, link)
+    for result in results_backups:
+        print(result)
